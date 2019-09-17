08df9e8c3d8a4078292a98aeab893e92178c7b4e
Nguyễn Ngọc Trung Hiếu <hieupoo1602@gmail.com>
2019-09-17 13:29:39 +0700

    Create ass9.cpp

---
+++ ass9.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+	 char n[10];
+	char str[1];
+	
+	
+	for(int i=0;i<10;i++){
+		scanf("%s",n[i]);
+	}
+	printf("nhap str");
+	
+	scanf("%s",str);
+	
+			if(strcmp(n,str)==0){printf("co thuoc\n	");
+}
+	
+	
+	
+	
+	
+	
+	return 0;
+	}
+	
+	
+	
+	
+
+
